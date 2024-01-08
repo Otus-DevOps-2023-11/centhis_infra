838fb05433d0a74eae2e40d903755a5fc232327c
Sergey Papulov <38090410+centhis@users.noreply.github.com>
2024-01-08 21:52:59 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
-
+  zone                     = var.zone
 }
 resource "yandex_compute_instance" "app" {
   count = var.count-reddit-apps
